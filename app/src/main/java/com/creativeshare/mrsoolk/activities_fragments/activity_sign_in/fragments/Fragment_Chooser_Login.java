08756c602c0059c9e2cd8b448fca7d315bf47a7d
AHMED <ahmedmohamed23345@gmail.com>
2019-06-30 09:33:08 +0200

    change

--- app/src/main/java/com/creativeshare/mrsoolk/activities_fragments/activity_sign_in/fragments/Fragment_Chooser_Login.java
+++ app/src/main/java/com/creativeshare/mrsoolk/activities_fragments/activity_sign_in/fragments/Fragment_Chooser_Login.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,9 +18,9 @@ import com.creativeshare.mrsoolk.activities_fragments.activity_sign_in.activity.
 public class Fragment_Chooser_Login extends Fragment {
 
     private LinearLayout ll_phone_sign_up;
-    private Button btn_skip,btn_terms;
+    private Button btn_skip;
     private SignInActivity activity;
-
+private TextView tv_terms;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,7 +38,7 @@ public class Fragment_Chooser_Login extends Fragment {
         activity = (SignInActivity) getActivity();
         ll_phone_sign_up = view.findViewById(R.id.ll_phone_sign_up);
         btn_skip = view.findViewById(R.id.btn_skip);
-        btn_terms = view.findViewById(R.id.btn_terms);
+        tv_terms = view.findViewById(R.id.tv_terms);
 
         ll_phone_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +47,7 @@ public class Fragment_Chooser_Login extends Fragment {
             }
         });
 
-        btn_terms.setOnClickListener(new View.OnClickListener() {
+       tv_terms.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 activity.NavigateToTermsActivity();
