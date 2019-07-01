852251126096f3922b10ab1f4fc99eb76f208461
AHMED <ahmedmohamed23345@gmail.com>
2019-07-01 15:17:43 +0200

    ch

---
+++ app/src/main/java/com/creativeshare/humhum/activities_fragments/activity_sign_in/fragments/Fragment_Chooser_Login.java
@@ -0,0 +1,63 @@
+package com.creativeshare.humhum.activities_fragments.activity_sign_in.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.creativeshare.humhum.R;
+import com.creativeshare.humhum.activities_fragments.activity_sign_in.activity.SignInActivity;
+
+public class Fragment_Chooser_Login extends Fragment {
+
+    private LinearLayout ll_phone_sign_up;
+    private Button btn_skip;
+    private SignInActivity activity;
+private TextView tv_terms;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_chooser_login,container,false);
+        initView(view);
+        return view;
+    }
+
+    public static Fragment_Chooser_Login newInstance()
+    {
+        return new Fragment_Chooser_Login();
+    }
+
+    private void initView(View view) {
+        activity = (SignInActivity) getActivity();
+        ll_phone_sign_up = view.findViewById(R.id.ll_phone_sign_up);
+        btn_skip = view.findViewById(R.id.btn_skip);
+        tv_terms = view.findViewById(R.id.tv_terms);
+
+        ll_phone_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.DisplayFragmentPhone();
+            }
+        });
+
+       tv_terms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.NavigateToTermsActivity();
+            }
+        });
+        btn_skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.NavigateToClientHomeActivity();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/creativeshare/humhum/activities_fragments/activity_splash/SplashActivity.java
@@ -0,0 +1,80 @@
+package com.creativeshare.humhum.activities_fragments.activity_splash;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.creativeshare.humhum.R;
+import com.creativeshare.humhum.activities_fragments.activity_home.client_home.activity.ClientHomeActivity;
+import com.creativeshare.humhum.activities_fragments.activity_sign_in.activity.SignInActivity;
+import com.creativeshare.humhum.language.Language_Helper;
+import com.creativeshare.humhum.models.UserModel;
+import com.creativeshare.humhum.preferences.Preferences;
+import com.creativeshare.humhum.singletone.UserSingleTone;
+import com.creativeshare.humhum.tags.Tags;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Preferences preferences;
+
+    private ImageView cov;
+    private String current_lang;
+    private LinearLayout lin;
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(Language_Helper.updateResources(base,Language_Helper.getLanguage(base)));
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        preferences = Preferences.getInstance();
+        cov=findViewById(R.id.img_cov);
+        lin=findViewById(R.id.lin);
+        lin.setVisibility(View.VISIBLE);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.move);
+        cov.startAnimation(animation);
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                lin.setVisibility(View.GONE);
+
+                String session = preferences.getSession(SplashActivity.this);
+
+                if (session.equals(Tags.session_login))
+                {
+                    UserModel userModel = preferences.getUserData(SplashActivity.this);
+                    UserSingleTone userSingleTone = UserSingleTone.getInstance();
+                    userSingleTone.setUserModel(userModel);
+
+                    Intent intent = new Intent(SplashActivity.this, ClientHomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else
+                    {
+                        Intent intent = new Intent(SplashActivity.this, SignInActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
